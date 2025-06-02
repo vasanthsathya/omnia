5e6c4f4710b3c0f466f91439da21c05be467b383
aditya-deshpande <aditya.deshpande1@dell.com>
2025-06-02 12:35:07 +0530

    Service node add hosts update

--- common/library/modules/get_telemetry_host_inventory.py
+++ common/library/modules/get_telemetry_host_inventory.py
@@ -19,10 +19,18 @@
 import csv
 from ansible.module_utils.basic import AnsibleModule
 
-
 def fetch_parent_tags(file_path):
-    parent_tags = set()
-    with open(file_path, newline='') as csvfile:
+    """
+    This function fetches the parent tags from a given CSV file.
+
+    Parameters:
+        file_path (str): The path to the CSV file containing parent tags.
+
+    Returns:
+        set: A set of parent tags found in the CSV file.
+    """
+    parent_tags = set()    
+    with open(file_path, newline='', encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             parent = row.get('PARENT', '').strip()
@@ -33,13 +41,56 @@ def fetch_parent_tags(file_path):
 
 
 def fetch_admin_ip(parent_tags, service_node_metadata):
-    """Validates input against rules."""
-    service_node_admin_ip = {}
-    for service_tag,data in service_node_metadata.items():
-        if service_tag in parent_tags:
-            service_node_admin_ip[service_tag] = data['admin_ip']
-    return service_node_admin_ip
+    """
+    Fetches the admin IP and virtual IP addresses of service nodes based on the provided parent tags and service node metadata.
 
+    Parameters:
+        parent_tags (list): A list of parent tags used to identify service nodes.
+        service_node_metadata (dict): A dictionary containing metadata about service nodes, including admin IP and virtual IP addresses.
+
+    Returns:
+        tuple: A tuple containing two dictionaries. The first dictionary maps service tags to their corresponding admin IP addresses.
+               The second dictionary maps service tags to their corresponding virtual IP addresses.
+    """
+    service_node_admin_ip = {}
+    service_node_virtual_ip = {}
+    active_nodes, passive_nodes = fetch_active_passive_serivetags(
+        parent_tags, service_node_metadata)
+    for nodes in (active_nodes, passive_nodes):
+        for service_tag in nodes:
+            service_node_admin_ip[service_tag] = service_node_metadata[service_tag]['admin_ip']
+
+    for nodes in active_nodes:
+        for service_tag in nodes:
+            service_node_virtual_ip[service_tag] = \
+                service_node_metadata[service_tag]['virtual_ip_address']
+
+    return service_node_admin_ip, service_node_virtual_ip
+
+def fetch_active_passive_serivetags(parent_tags, service_node_metadata):
+    """
+    Fetches the active and passive service tags based 
+    on the provided parent tags and service node metadata.
+
+    Parameters:
+        parent_tags (list): A list of parent tags used to identify service nodes.
+        service_node_metadata (dict): A dictionary containing metadata about service nodes.
+
+    Returns:
+        tuple: A tuple containing two sets. The first set contains active service tags.
+               The second set contains passive service tags.
+    """
+    active_nodes = set()
+    passive_nodes = set()
+    for service_tag, data in service_node_metadata.items():
+        if service_tag in parent_tags:
+            if data['enable_service_ha']:
+                if data['active']:
+                    active_nodes.add(service_tag)
+                    passive_nodes.update(data['passive_nodes'])
+                else:
+                    passive_nodes.add(service_tag)
+    return active_nodes, passive_nodes
 
 def main():
     """Main module function."""
@@ -53,8 +104,8 @@ def main():
     service_node_metadata = module.params["service_node_metadata"]
     try:
         parent_tags = fetch_parent_tags(bmc_group_data_path)
-        sn_admin_ip = fetch_admin_ip(parent_tags, service_node_metadata)
-        module.exit_json(changed=False, sn_admin_ip=sn_admin_ip)
+        sn_admin_ip, sn_virtual_ip = fetch_admin_ip(parent_tags, service_node_metadata)
+        module.exit_json(changed=False, sn_admin_ip=sn_admin_ip, sn_virtual_ip=sn_virtual_ip)
     except ValueError as e:
         module.fail_json(msg=f"Failed to to get Service Node Group data. {e}")
 
