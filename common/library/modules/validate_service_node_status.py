5177f357ea8915666086cc20882cebbcdfde502d
Manasa.Hemmanur <Manasa.Hemmanur@dell.com>
2025-06-10 23:24:18 +0530

    implemented validation for federated idrac collection

--- common/library/modules/validate_service_node_status.py
+++ common/library/modules/validate_service_node_status.py
@@ -43,38 +43,93 @@ def load_yaml(path):
     with open(path, "r", encoding = "utf-8") as file:
         return yaml.safe_load(file)
 
-def check_service_node_entry(roles_config):
+def is_service_node_defined(roles_config):
+    """
+    Checks if a 'service_node' entry exists in the roles configuration.
+
+    Args:
+        roles_config (str): The path to the roles configuration file.
+
+    Returns:
+        bool: True if a 'service_node' entry is found, False otherwise.
+
+    Notes:
+        This function loads the roles configuration from the specified file and checks
+        for the presence of a 'service_node' role.
+    """
+
     roles_config_json = load_yaml(roles_config)
 
     roles_configured = roles_config_json.get('Roles',[])
 
-    return next((role for role in roles_configured if role.get('name') == "service_node"), None)
+    return any(role.get('name') == "service_node" for role in roles_configured)
 
-def check_and_get_service_nodes_status(bmc_group_data_path, service_nodes_not_booted, service_nodes_booted):
-    nodes_not_booted = []
-    role = "service_node"
-    conn = omniadb.create_connection()
-    cursor = conn.cursor()
-    cursor.execute(
-        "SELECT bmc_ip, status FROM cluster.nodeinfo WHERE role = %s",
-            (role,)
-    )
-    records = cursor.fetchall()
-    cursor.close()
-    conn.close()
 
-    if records:
-        records_dict = dict(records)
-        with open(bmc_group_data_path, newline='', encoding='utf-8') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                bmc_ip = row.get('BMC_IP', '').strip()
-                node_status = records_dict.get(bmc_ip)
+def check_and_get_service_nodes_status(
+    bmc_group_data_path,
+    service_nodes_not_booted,
+    service_nodes_booted,
+    module
+):
+    """
+    Checks the status of service nodes and updates the lists of booted and not booted nodes.
 
-                if node_status is not None and node_status != "booted":
-                    service_nodes_not_booted.append(bmc_ip)
-                elif node_status is not None and node_status == "booted":
-                    service_nodes_booted.append(bmc_ip)
+    This function queries the database for the status of service nodes,
+    reads the BMC group data from a file,
+    and updates the lists of booted and not booted service nodes based on the node status.
+
+    Args:
+        bmc_group_data_path (str): The path to the BMC group data file.
+        service_nodes_not_booted (list): A list to store the BMC IPs of service nodes
+                                        that are not booted.
+        service_nodes_booted (list): A list to store the BMC IPs of service nodes that are booted.
+
+    Returns:
+        None
+
+    Notes:
+        This function assumes that the database connection is established and
+        the BMC group data file is in the correct format.
+        The function updates the input lists `service_nodes_not_booted` and `service_nodes_booted`
+        with the BMC IPs of service nodes that are not booted and booted, respectively.
+    """
+
+    try:
+        role = "service_node"
+        conn = omniadb.create_connection()
+        cursor = conn.cursor()
+        try:
+            cursor.execute(
+                "SELECT bmc_ip, status FROM cluster.nodeinfo WHERE role = %s",
+                (role,)
+            )
+            records = cursor.fetchall()
+        except Exception as e:
+            print(f"Error: Failed to execute query. {e}")
+            return
+        finally:
+            cursor.close()
+            conn.close()
+
+        if records:
+            records_dict = {record[0]: record[1] for record in records}
+            try:
+                with open(bmc_group_data_path, newline='', encoding='utf-8') as csvfile:
+                    reader = csv.DictReader(csvfile)
+                    for row in reader:
+                        bmc_ip = row.get('BMC_IP', '').strip()
+                        node_status = records_dict.get(bmc_ip)
+
+                        if node_status is not None and node_status != "booted":
+                            service_nodes_not_booted.append(bmc_ip)
+                        elif node_status is not None and node_status == "booted":
+                            service_nodes_booted.append(bmc_ip)
+            except FileNotFoundError:
+                module.fail_json(msg="File {%s} not found" % (bmc_group_data_path))
+            except csv.Error as e:
+                module.fail_json(msg=f"Error: Failed to read file '{bmc_group_data_path}'. {e}")
+    except Exception as e:
+        module.warn(f"Error: Failed to create database connection. {e}")
 
 def main():
     """Main module function."""
@@ -93,16 +148,20 @@ def main():
 
     try:
 
-        roles_config = input_dir + "/roles_config.py"
-        service_node_defined = check_service_node_entry(roles_config)
+        roles_config = input_dir + "/roles_config.yml"
+        service_node_defined = is_service_node_defined(roles_config)
 
         if service_node_defined:
-            check_and_get_service_nodes_status(bmc_group_data_path, service_nodes_not_booted, service_nodes_booted)
+            check_and_get_service_nodes_status(bmc_group_data_path,
+                                               service_nodes_not_booted,
+                                               service_nodes_booted, module)
 
-        module.exit_json(changed=False, service_node_defined=service_node_defined,
-                         service_nodes_not_booted=service_nodes_not_booted, service_nodes_booted=service_nodes_booted)
+        module.exit_json(changed=False,
+                         service_node_defined=service_node_defined,
+                         service_nodes_not_booted=service_nodes_not_booted,
+                         service_nodes_booted=service_nodes_booted)
     except ValueError as e:
-        module.fail_json(msg=f"Failed to to get Service Node Group data. {e}")
+        module.fail_json(msg=f"Failed to to get Service Node data. {e}")
 
 if __name__ == "__main__":
     main()
