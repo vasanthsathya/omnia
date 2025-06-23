85cf464273a35123dc59dd18a09f64e8eda4e8c1
priti-parate <140157516+priti-parate@users.noreply.github.com>
2025-06-24 03:45:51 +0530

    disable unused-argument due to design limitation

--- common/library/module_utils/input_validation/validation_flows/common_validation.py
+++ common/library/module_utils/input_validation/validation_flows/common_validation.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# pylint: disable=import-error,no-name-in-module
+# pylint: disable=import-error,no-name-in-module,too-many-arguments,Unused argument
 """
 This module contains functions for validating common configuration files.
 """
