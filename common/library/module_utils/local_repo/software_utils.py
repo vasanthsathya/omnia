0ab18b325c241f05ffe400cb4639d5fd5185ccf9
Vrinda_Marwah <vrinda.marwah@dell.com>
2025-06-26 09:17:16 +0000

    handle None values in software package parsing
    Signed-off-by: Vrinda_Marwah <Vrinda.Marwah@dell.com>

--- common/library/module_utils/local_repo/software_utils.py
+++ common/library/module_utils/local_repo/software_utils.py
@@ -396,7 +396,6 @@ def get_csv_software(file_name):
         reader = csv.DictReader(csv_file)
         csv_software = [row.get(CSV_COLUMNS["column1"], "").strip()
                         for row in reader]
-
     return csv_software
 
 
@@ -417,9 +416,11 @@ def get_failed_software(file_name):
 
     with open(file_name, mode='r') as csv_file:
         reader = csv.DictReader(csv_file)
-        failed_software = [row.get(CSV_COLUMNS["column1"], "").strip()
-                           for row in reader
-                           if row.get(CSV_COLUMNS["column2"], "").strip().lower() == "failed"]
+        failed_software = [
+            str(row.get(CSV_COLUMNS["column1"]) or "").strip()
+            for row in reader
+            if str(row.get(CSV_COLUMNS["column2"]) or "").strip().lower() in ["", "failed"]
+        ]
 
     return failed_software
 
