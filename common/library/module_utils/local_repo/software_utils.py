9bbaa63caa8d12322ef501f084fb39894ee4c2cd
balajikumaran.cs <balajikumaran.c.s@gmail.com>
2025-06-26 15:48:08 +0530

    Update software_utils.py
    Signed-off-by: balajikumaran.cs <balajikumaran.c.s@gmail.com>

--- common/library/module_utils/local_repo/software_utils.py
+++ common/library/module_utils/local_repo/software_utils.py
@@ -11,14 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-<<<<<<< pub/local_repo
 # pylint: disable=import-error,no-name-in-module
-=======
-
-# pylint: disable=line-too-long,import-error,no-name-in-module
 
 from collections import defaultdict
->>>>>>> staging
 import os
 import json
 import csv
