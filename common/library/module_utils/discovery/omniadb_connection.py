16f7dc664151eb35919e448aac9d00c69ccf1061
Milisha Gupta <52577117+Milisha-Gupta@users.noreply.github.com>
2025-07-03 17:16:10 +0530

    Update omniadb_connection.py
    Signed-off-by: Milisha Gupta <52577117+Milisha-Gupta@users.noreply.github.com>

--- common/library/module_utils/discovery/omniadb_connection.py
+++ common/library/module_utils/discovery/omniadb_connection.py
@@ -11,6 +11,9 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+
+# pylint: disable=import-error,no-name-in-module,line-too-long
+
 """
 Database connection utilities for Omnia discovery modules.
 """
