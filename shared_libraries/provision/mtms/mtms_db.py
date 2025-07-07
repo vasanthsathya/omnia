52d5fcaf9ffe97345ef2f5a452d9f9f5bd4750ad
Milisha Gupta <52577117+Milisha-Gupta@users.noreply.github.com>
2025-07-07 17:29:42 +0530

    Update mtms_db.py
    Signed-off-by: Milisha Gupta <52577117+Milisha-Gupta@users.noreply.github.com>

--- shared_libraries/provision/mtms/mtms_db.py
+++ shared_libraries/provision/mtms/mtms_db.py
@@ -11,6 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License
+#pylint: disable=import-error,no-name-in-module
 """This module contains functions for updating db based on mtms."""
 import ipaddress
 import sys
